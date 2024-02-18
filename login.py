701d1b4095c8180909e8616c471278a0300f1370
David Lang <davidlang42@gmail.com>
2024-02-18 23:22:12 +1100

    Remove more unnessesary code

--- login.py
+++ login.py
@@ -11,7 +11,6 @@ if not g_pass:
     print("Must set environment variable GOOGLE_APP_PASSWORD, see https://myaccount.google.com/apppasswords")
     exit(1)
 
-gkeepapi.node.DEBUG = True
 keep = gkeepapi.Keep()
 keep.login(g_user, g_pass)
 print(keep.getMasterToken())
